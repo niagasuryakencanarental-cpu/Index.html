11be49800d8cfbe4e0f1a15cd16f2005e6e035c0
niagasuryakencanarental-cpu <niagasuryakencanarental@gmail.com>
2026-04-09 23:36:09 +0700

    Initialize Firebase app with configuration
    Added Firebase initialization script with configuration.

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,11 @@
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
+  const firebaseConfig = {
+    apiKey: "ISI_PUNYAMU",
+    authDomain: "ISI_PUNYAMU",
+    projectId: "ISI_PUNYAMU",
+  };
+
+  const app = initializeApp(firebaseConfig);
+</script>
